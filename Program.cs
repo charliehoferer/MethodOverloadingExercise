ef48cff3e1994d4c547d01385ad7db9f9cc08a61
Charlie Hoferer <charlie.hoferer@gmail.com>
2021-04-07 17:18:12 -0700

    second commit

--- Program.cs
+++ Program.cs
@@ -6,31 +6,32 @@ namespace MethodOverloadingExercise
     {
         static void Main()
         {
-            
+            var num1 = 2;
+            var num2 = 5;
+            var dec1 = 2.1m;
+            var dec2 = 3.2m;
+            var num3 = Decimal.ToInt32(dec1);
+            var num4 = Decimal.ToInt32(dec2);
+            Console.WriteLine("Please give a number...");
+            var num5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please give another number...");
+            var num6 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Is this true or false?");
+            var truth1 = Convert.ToBoolean(Console.ReadLine());
         }
         static int Add(int num1, int num2)
         {
-            num1 = 2;
-            num2 = 5;
             return num1 + num2;
         }
-        static int Add(int num1, int num2, decimal dec1, decimal dec2)
+        static int Add(int num1, int num2, int num3, int num4, decimal dec1, decimal dec2)
         {
-            dec1 = 2.1m;
-            dec2 = 3.2m;
-            int num3 = Decimal.ToInt32(dec1);
-            int num4 = Decimal.ToInt32(dec2);
+            
             return num1 + num2 + num3 + num4;
         }
 
         static int Add(int num1, int num2, int num3, int num4, int num5, int num6, decimal dec1, decimal dec2, bool truth1)
         {
-            Console.WriteLine("Please give a number...");
-            num5 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please give another number...");
-            num6 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Is this true or false?");
-            truth1 = Convert.ToBoolean(Console.ReadLine());
+            
             return num5 + num6;
             int sum = num5 + num6;
             if (truth1 == true )
